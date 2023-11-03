6f4c8f376c0a9be6b251502dc81ac109e7e3c4ed
FrancescoPepi <francescopepi16@gmail.com>
2023-11-03 17:33:12 +0100

    x bugs

--- myphp.php
+++ myphp.php
@@ -1,38 +1,41 @@
 <?php
-$key=$_GET['key'];
-$str=$_GET['str'];
-$return=str_replace($key,"***",$str);
+$key = $_GET['key'];
+$str = $_GET['str'];
+$return = str_replace($key, "***", $str);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Censured</title>
 </head>
+
 <body>
 
-<div>
-    <h2>Parola da censurare</h2>
-<?php 
-echo $key ?>
-</div>
-<div>
-    <h2>Paragrafo</h2>
-<?php
-echo "La frase iniziale è: ".$str ?>
-</div>
+    <div>
+        <h2>Parola da censurare</h2>
+        <?php
+        echo $key ?>
+    </div>
+    <div>
+        <h2>Paragrafo</h2>
+        <?php
+        echo "La frase iniziale è: " . $str ?>
+    </div>
 
-<div>    
-<?php 
-echo "E' lunga: ".strlen($str)." caratteri" ?>
-</div>
-<hr>
-<div>    
-<?php 
-echo $return ?>
-</div>
-    
+    <div>
+        <?php
+        echo "E' lunga: " . strlen($str) . " caratteri" ?>
+    </div>
+    <hr>
+    <div>
+        <?php
+        echo $return ?>
+    </div>
+
 </body>
+
 </html>
